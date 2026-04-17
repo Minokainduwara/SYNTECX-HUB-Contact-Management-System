a9d9868719b7b395568ee83315a95c1df6c74a67
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-04-18 04:17:59 +0530

    Feat(server): import Env

---
+++ backend/server.ts
@@ -0,0 +1,18 @@
+import app from "./src/app";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5007;
+
+mongoose.connect(process.env.MONGO_URI!)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error("Failed to connect to MongoDB", err);
+        process.exit(1);
+    });
