773eef168ef8496e188a19c53b0e8879d1b13d08
Minoka Wickramasinghe <76654300+Minokainduwara@users.noreply.github.com>
2026-04-19 02:03:17 +0530

    Fix: Server errors fixed

--- backend/server.ts
+++ backend/server.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 
 dotenv.config();
-const PORT = process.env.PORT || 5007;
+const PORT = process.env.PORT;
 
 mongoose.connect(process.env.MONGO_URI!)
     .then(() => {
--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import contactRoutes from "./routes/routes";
+import contactRoutes from "./routes/contact.routes";
 
 
 const app = express();
